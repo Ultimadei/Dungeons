c849e2d41a9b24a37e623638a013f410fab7a151
Samuel Shakeshaft <sam.shakeshaft@outlook.com>
2022-03-26 20:51:58 +0000

    Experimentation and skeleton setup

--- Dungeons/main.cpp
+++ Dungeons/main.cpp
@@ -2,26 +2,49 @@
 
 class Game : public olcConsoleGameEngine {
 public:
-	Game() {
-
-	}
+	Game() {}
 
 	// Inherited via olcConsoleGameEngine
 	virtual bool OnUserCreate() override
 	{
+		ClearCanvas();
+
 		return true;
 	}
 
 	virtual bool OnUserUpdate(float fElapsedTime) override
 	{
+		if (m_mouse[0].bPressed) { m_bPainting = true; }
+		else if (m_mouse[0].bReleased) { m_bPainting = false; }
+
+		if (m_bPainting) {
+			m_paintCanvas.SetColour(m_mousePosX, m_mousePosY, FG_WHITE);
+			m_paintCanvas.SetGlyph(m_mousePosX, m_mousePosY, PIXEL_SOLID);
+		}
+
+		// User requested to clear the canvas
+		if (m_keys['C'].bPressed) {
+			ClearCanvas();
+		}
+
 		// Clear the screen
-		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+		Fill(0, 0, m_nScreenWidth, m_nScreenHeight, PIXEL_SOLID, FG_BLACK);
 
-
-		Draw(GetMouseX(), GetMouseY(), PIXEL_SOLID, FG_WHITE);
+		// Draw the canvas (excludes the empty space)
+		DrawPartialSprite(0, 0, &m_paintCanvas, 0, 0, m_nScreenWidth, m_nScreenHeight);
 
 		return true;
 	}
+
+private:
+
+	void ClearCanvas() {
+		m_paintCanvas = olcSprite(m_nScreenWidth, m_nScreenHeight);
+	}
+
+private:
+	olcSprite m_paintCanvas;
+	bool m_bPainting;
 };
 
 int main(int argc, char** argv) {
